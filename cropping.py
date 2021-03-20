ef08b2c775e400d904774c3bf893f3a28d37598c
Siroyan <1517053@ed.tus.ac.jp>
2021-03-20 18:56:32 +0900

    とりあえず完成

--- cropping.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-im = Image.open('./assets/ranking_number.png')
-
-im.crop((0, 0, 227, 227)).save('no1.png', quality=95)
-im.crop((343, 0, 343 + 227, 227)).save('no2.png', quality=95)
-im.crop((343 + 343, 0, 343 + 343 + 227, 227)).save('no3.png', quality=95)
